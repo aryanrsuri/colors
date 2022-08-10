b316952966c2f9fddee1767545dd156b215f2900
Aryan <aryanrsuri@gmail.com>
2022-08-09 22:21:14 -0700

    welp

--- index.ts
+++ index.ts
@@ -2,11 +2,29 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
 const HTML = await Deno.readFile("./public/index.html");
-const CSS = await Deno.readFile("./public/style.css");
+const CSS = await Deno.readFile("./public/styles.css");
+const JS = await Deno.readFile("./public/index.js");
 serve(async () => {
   return new Response(HTML, {
     headers: new Headers({
       "content-type": "text/html",
     }),
   });
+});
+
+serve(async () => {
+  return new Response(CSS, {
+    headers: new Headers({
+      "content-type": "text/css",
+    }),
+  });
+});
+
+serve(async () => {
+    return new Response(JS, {
+        headers: new Headers({
+        "content-type": "text/javascript",
+        }),
+    });
+    }
 });
