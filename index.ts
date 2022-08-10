b9ec7d79e7c2cfe0a96c7eef3263fb848eb852a8
Aryan <aryanrsuri@gmail.com>
2022-08-09 22:14:55 -0700

    0.0.3

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
 const HTML = await Deno.readFile("./public/index.html");
-
+const CSS = await Deno.readFile("./public/style.css");
 serve(async () => {
   return new Response(HTML, {
     headers: new Headers({
